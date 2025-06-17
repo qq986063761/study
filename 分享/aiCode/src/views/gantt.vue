62f98a45110a3d7ca42795762365a7cc312bba16
wanpeng <986063761@qq.com>
2025-06-17 18:39:05 +0800

    更新

--- 分享/aiCode/src/views/gantt.vue
+++ 分享/aiCode/src/views/gantt.vue
@@ -2,7 +2,7 @@
   <div class="gantt-demo">
     <h1>甘特图演示</h1>
     <!-- 这里引入甘特图组件 -->
-    <GanttChart />
+    <GanttChart class="gantt-component" :tasks="tasks" />
   </div>
 </template>
 
@@ -15,12 +15,32 @@ export default {
   components: {
     GanttChart,
   },
+  data() {
+    return {
+      tasks: [
+        {
+          startTime: new Date(2025, 5, 17).getTime(),
+          endTime: new Date(2025, 5, 19).getTime(),
+          name: '任务1'
+        },
+        {
+          startTime: new Date(2025, 5, 14).getTime(),
+          endTime: new Date(2025, 5, 17).getTime(),
+          name: '任务2'
+        }
+      ]
+    }
+  }
 };
 </script>
 
-<style scoped>
+<style lang="scss">
 .gantt-demo {
   padding: 20px;
   text-align: center;
+
+  .gantt-component {
+    height: 500px;
+  }
 }
 </style>
