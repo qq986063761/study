ac4111551e48d793cb7b555b7012006318e83481
wanpeng <986063761@qq.com>
2025-07-02 23:19:01 +0800

    更新

--- 分享/aiCode/src/components/password/password.vue
+++ 分享/aiCode/src/components/password/password.vue
@@ -4,18 +4,16 @@
     <div class="subtitle">请输入交易密码</div>
     <div class="inputs">
       <input
-        v-for="(num, idx) in passwordArr"
+        v-for="(num, idx) in 6"
         :key="idx"
-        ref="inputs"
         type="password"
         maxlength="1"
-        inputmode="numeric"
-        pattern="[0-9]*"
-        v-model="passwordArr[idx]"
-        @input="onInput(idx, $event)"
+        ref="inputs"
+        v-model="password[idx]"
+        @input="onInput(idx)"
         @keydown.backspace="onBackspace(idx, $event)"
         @focus="onFocus(idx)"
-        :class="{ active: currentIdx === idx }"
+        autocomplete="off"
       />
     </div>
     <div class="forgot" @click="onForgot">忘记密码?</div>
@@ -26,31 +24,29 @@
 export default {
   data() {
     return {
-      passwordArr: ['', '', '', '', '', ''],
-      currentIdx: 0,
+      password: ['', '', '', '', '', ''],
     };
   },
   methods: {
-    onInput(idx, e) {
-      const val = e.target.value.replace(/\D/g, '');
-      this.$set(this.passwordArr, idx, val);
-      if (val && idx < 5) {
-        this.focusInput(idx + 1);
+    onInput(idx) {
+      // 只允许输入数字
+      this.password[idx] = this.password[idx].replace(/\D/g, '');
+      if (this.password[idx] && idx < 5) {
+        this.$refs.inputs[idx + 1].focus();
       }
-      if (this.passwordArr.join('').length === 6) {
-        this.$emit('complete', this.passwordArr.join(''));
+      // 输入完成
+      if (this.password.join('').length === 6) {
+        this.$emit('complete', this.password.join(''));
       }
     },
     onBackspace(idx, e) {
-      if (!this.passwordArr[idx] && idx > 0) {
-        this.focusInput(idx - 1);
+      if (!this.password[idx] && idx > 0) {
+        this.$refs.inputs[idx - 1].focus();
       }
     },
     onFocus(idx) {
-      this.currentIdx = idx;
-    },
-    focusInput(idx) {
-      this.$refs.inputs[idx].focus();
+      // 聚焦时自动选中内容
+      this.$refs.inputs[idx].select();
     },
     onForgot() {
       this.$emit('forgot');
@@ -72,7 +68,7 @@ export default {
   margin-bottom: 8px;
 }
 .subtitle {
-  font-size: 18px;
+  font-size: 16px;
   color: #666;
   margin-bottom: 32px;
 }
@@ -90,9 +86,7 @@ export default {
   border-radius: 8px;
   outline: none;
   transition: border 0.2s;
-  background: #fff;
 }
-.inputs input.active,
 .inputs input:focus {
   border: 2px solid #409eff;
 }
@@ -101,7 +95,6 @@ export default {
   font-size: 16px;
   cursor: pointer;
   margin-top: 16px;
-  user-select: none;
 }
 .forgot:hover {
   color: #409eff;
